d30d8f698b6fef3c6350ba469bc324038b907ee1
Ryan Duncan <weeryzer@gmail.com>
2017-04-20 01:44:48 +0100

    updates to gr2 - all done apart from responsiveness

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
       }
     },
     "color": {
-      "value": "#ffffff"
+      "value": "#daeff3"
     },
     "shape": {
       "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#ffffff",
+      "color": "#daeff3",
       "opacity": 0.4,
       "width": 1
     },
